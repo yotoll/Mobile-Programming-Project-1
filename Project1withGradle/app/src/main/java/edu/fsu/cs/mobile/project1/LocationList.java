22a6103ed5b907a1d73ae6c26f271c3380185cc0
Abi <ajanpal@gmail.com>
2018-07-17 19:46:47 -0400

    Add files via upload

--- Project1withGradle/app/src/main/java/edu/fsu/cs/mobile/project1/LocationList.java
+++ Project1withGradle/app/src/main/java/edu/fsu/cs/mobile/project1/LocationList.java
@@ -2,6 +2,8 @@
 package edu.fsu.cs.mobile.project1;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
@@ -24,15 +26,15 @@ import edu.fsu.cs.mobile.project1.R;
 public class LocationList extends ListFragment {
 
     ListView list;
-    public ArrayList<String> locations;
+    public ArrayList<String> locations, contacts;
     public ArrayAdapter<String> adapt;
+
     String messageContent;
     String messageSender;
 
-    public Button test;
-
     private int taken;
     public static final String TAG = LocationList.class.getCanonicalName();
+    public static final String Prefs = "RecentLocFile";
 
     public LocationList() {
         // Required empty public constructor
@@ -44,78 +46,177 @@ public class LocationList extends ListFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_location_list, container, false);
+
+        //Recent location and contact it was from
         locations = new ArrayList<String>();
-        locations.add("No results found");
+        contacts = new ArrayList<String>();
+
+
+        //Reading from SharedPreferences for previous locations
+        SharedPreferences settings = getActivity().getSharedPreferences(Prefs, 0);
+
+        taken = settings.getInt("Taken", 0);
+        if (taken > 0)
+        {
+            locations.add(settings.getString("FirstLoc", "Empty"));
+            contacts.add(settings.getString("FirstCont", "No Results found"));
+            if(taken>1)
+            {
+                locations.add(settings.getString("SecondLoc", "Empty"));
+                contacts.add(settings.getString("SecondCont", "No Results found"));
+            }
+            if(taken>2)
+            {
+                locations.add(settings.getString("ThirdLoc", "Empty"));
+                contacts.add(settings.getString("ThirdCont", "No Results found"));
+
+            }
+            if(taken>3)
+            {
+                locations.add(settings.getString("FourthLoc", "Empty"));
+                contacts.add(settings.getString("FourthCont", "No Results found"));
+            }
+            if(taken>4)
+            {
+                locations.add(settings.getString("FifthLoc", "Empty"));
+                contacts.add(settings.getString("FifthCont", "No Results found"));
+            }
+        }
+        else
+        {
+            contacts.add("No results found");
+        }
+
         adapt = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1, locations);
+                android.R.layout.simple_list_item_1, contacts);
         taken = 0;
         setListAdapter(adapt);
         list = new ListView(getActivity());
-        
+
         // Phone Number and Message from BroadCast Receiver
         Bundle extras = getActivity().getIntent().getExtras();
         if (extras != null) {
             messageContent = extras.getString("msgContent");
             messageSender = extras.getString("phoneNum");
+            addResult(messageContent,messageSender);
 
         }
-
 
         return v;
     }
 
+
+    //Send user over to recent location
     @Override
     public void onListItemClick(ListView l, View v, int pos, long id)
     {
         super.onListItemClick(l,v,pos,id);
+        double lat, lon;
+
         if(taken > 0)
         {
-            Toast.makeText(getActivity(), "Clicked!", Toast.LENGTH_SHORT).show();
             //pull up location
-            locations.get(pos);
+            String del = "[ ]+";
+            String [] temp = locations.get(pos).split(del);
+
+            lat = Double.parseDouble(temp[0]);
+            lon = Double.parseDouble(temp[1]);
+
+            Intent intent = new Intent(getContext(), MapsActivity.class);
+            intent.putExtra("LAT", lat);
+            intent.putExtra("LON", lon);
+            startActivity(intent);
+
         }
 
     }
 
-    public void addResult(String x)
+
+    //Add on to contact list
+    public void addResult(String loc, String con)
     {
-        Log.i("###LOCATION LIST###","addingResult");
+
+        String x, y;
+
+        x=loc;
+        y=con;
+
         if(taken==0)
         {
             //replace with location
             locations.add(0,x);
-            locations.remove(1);
+
+            contacts.add(0,y);
+            contacts.remove(1);
+
             taken++;
             adapt = new ArrayAdapter<String>(getActivity(),
-                    android.R.layout.simple_list_item_1, locations);
+                    android.R.layout.simple_list_item_1, contacts);
             setListAdapter(adapt);
         }
         else
         {
-            //push down one
-            //add to front
-
 
             locations.add(0,x);
+            contacts.add(0,y);
 
             adapt = new ArrayAdapter<String>(getActivity(),
-                    android.R.layout.simple_list_item_1, locations);
+                    android.R.layout.simple_list_item_1, contacts);
             setListAdapter(adapt);
+
             if(taken < 5)
                 taken++;
             else
+            {
                 locations.remove(5);
+                contacts.remove(5);
+            }
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
 
     @Override
     public void onDetach() {
+        //Write to Shared Prefs?
         super.onDetach();
+
+
+        //Write recent locations to the shared preferences
+        SharedPreferences preferences = getActivity().getSharedPreferences(Prefs,0);
+        SharedPreferences.Editor edit = preferences.edit();
+
+        edit.putInt("Taken",taken);
+
+        if(taken>0)
+        {
+            edit.putString("FirstLoc",locations.get(0));
+            edit.putString("FirstCont",contacts.get(0));
+        }
+
+        if(taken>1)
+        {
+            edit.putString("SecondLoc",locations.get(1));
+            edit.putString("SecondCont",contacts.get(1));
+        }
+
+        if(taken>2)
+        {
+            edit.putString("ThirdLoc",locations.get(2));
+            edit.putString("ThirdCont",contacts.get(2));
+        }
+
+        if(taken>3)
+        {
+            edit.putString("FourthLoc",locations.get(3));
+            edit.putString("FourthCont",contacts.get(3));
+        }
+
+        if(taken>4)
+        {
+            edit.putString("FifthLoc", locations.get(4));
+            edit.putString("FifthCont", contacts.get(4));
+        }
+        edit.commit();
     }
 
 }
