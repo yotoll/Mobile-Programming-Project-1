f94bd9a03cf56ab6992168ba436dfab1e0242c53
DESKTOP-N37AKD8\Dominick Aiudi <dra17d@my.fsu.edu>
2018-07-16 22:02:29 -0400

    Added a Map Activity that pulls up Google Maps with the device's location in-app when you press "Send" -------- Tested it on my phone, doesn't seem to work well with the emulators -------- Had to use an API key that was connected to my Google account so make your own API key and put it into "google_maps_api.xml" if you want to test the map function, if anyone can find an alternative to this, let me know --------- Using the txt file in the repo to list bugs

---
+++ Project1withGradle/app/src/main/java/edu/fsu/cs/mobile/project1/LocationReceiver.java
@@ -0,0 +1,15 @@
+package edu.fsu.cs.mobile.project1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class LocationReceiver extends BroadcastReceiver
+{
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
--- Project1withGradle/app/src/main/java/edu/fsu/cs/mobile/project1/MainActivity.java
+++ Project1withGradle/app/src/main/java/edu/fsu/cs/mobile/project1/MainActivity.java
@@ -2,15 +2,20 @@ package edu.fsu.cs.mobile.project1;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.EditText;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,33 +23,31 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    double lat;
+    double lon;
+
     EditText phoneNumber;
-    Button sendBtn;
     LocationManager locationManager;
     LocationListener locationListener;
 
-    String message="http://maps.google.com/maps?saddr=";//+lat+","+lon;
+    String messagebase="http://maps.google.com/maps?saddr=";//+lat+","+lon;
+    String messageupdate="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sendBtn = (Button) findViewById(R.id.sendButton);
         phoneNumber = (EditText) findViewById(R.id.numText);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-        sendBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                message = message + location.getLatitude()+ ","+location.getLongitude();
-                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+                messageupdate = messagebase + location.getLatitude()+ ","+location.getLongitude();
+                lat = location.getLatitude();
+                lon = location.getLongitude();
+                Toast.makeText(MainActivity.this, messageupdate, Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -65,11 +68,12 @@ public class MainActivity extends AppCompatActivity {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String [] {
                     Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.INTERNET
+                    Manifest.permission.INTERNET,Manifest.permission_group.SMS
             },7);
             return;
         }
         locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+
     }
 
     @Override
@@ -79,5 +83,17 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
                     phoneNumber.setText("something");
         }
+    }
+
+    /////////////////////////////////////////////////////////////
+    //Method to open map                                       //
+    /////////////////////////////////////////////////////////////
+
+    public void onMap (View v)
+    {
+        Intent intent = new Intent(this, MapsActivity.class);
+        intent.putExtra("LAT", lat);
+        intent.putExtra("LON", lon);
+        startActivity(intent);
     }
 }
