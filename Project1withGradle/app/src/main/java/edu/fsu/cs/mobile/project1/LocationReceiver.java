752d2e6ba88fcd09c2d65b4d66b1c1d1776c75b5
amarsh0 <39601527+amarsh0@users.noreply.github.com>
2018-07-16 23:39:52 -0400

    Update LocationReceiver.java

--- Project1withGradle/app/src/main/java/edu/fsu/cs/mobile/project1/LocationReceiver.java
+++ Project1withGradle/app/src/main/java/edu/fsu/cs/mobile/project1/LocationReceiver.java
@@ -10,6 +10,25 @@ public class LocationReceiver extends BroadcastReceiver
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        
+        if (intent.getAction().equals(SMS_RECEIVED)) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                Object[] pdus = (Object[])bundle.get("pdus");
+                final SmsMessage[] messages = new SmsMessage[pdus.length];
+                for (int i = 0; i < pdus.length; i++) {
+                    messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                }
+                if (messages.length > -1) {
+
+                    Intent inte = new Intent(context.getApplicationContext(), MainActivity.class);
+                    inte.putExtra("msgContent", messages[0].getMessageBody());
+                    inte.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // adding this flag starts the new Activity in a new Task
+                    context.startActivity(inte);
+                    Log.i(TAG, "Message received: " + messages[0].getMessageBody());
+
+                }
+            }
+        }
     }
 }
